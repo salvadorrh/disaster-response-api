953061a52cecf33539e84d9829b41ca74ac5f1b7
salvadorrh <salvadorroblesherrera@gmail.com>
2025-12-03 18:22:36 -0600

    Developed Backend API

--- app.py
+++ app.py
@@ -1,4 +1,43 @@
 # app.py
 from flask import Flask, request, jsonify
+import joblib
 
-import pickle 
+app = Flask(__name__)
+
+def load_model(path):
+    with open(path, "rb") as f:
+        return joblib.load(f)
+    
+# Load vectorizer
+vectorizer = load_model("models/final_vectorizer.pkl")
+
+# Lables -> ['aid_related', 'water', 'food', 'shelter']
+models = {
+    "aid_related": load_model("models/SGDClassifier_tfidf_5000_0.pkl"),
+    "water": load_model("models/SGDClassifier_tfidf_5000_1.pkl"),
+    "food": load_model("models/SGDClassifier_tfidf_5000_2.pkl"),
+    "shelter": load_model("models/SGDClassifier_tfidf_5000_3.pkl")
+}
+
+# Home route
+@app.route("/", methods=["GET"])
+def home():
+    return {"status": "alive"}
+
+# Prediction
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    text = data.get("text", "")
+    X = vectorizer.transform([text])
+
+    # For each label
+    results = {}
+    for label, model in models.items():
+        pred = int(model.predict(X)[0])
+        results[label] = pred
+
+    return jsonify(results)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 5000)
